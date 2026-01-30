803ae5b1b9f5b9354ae9c3929b4726a6a0bcdb98
Amit Mor Yosef <105875624+TheWazzzzzup@users.noreply.github.com>
2026-01-30 14:05:20 +0200

    Added Data Site Health

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -4,5 +4,9 @@ namespace DefaultNamespace
     {
         public static int AskQuestions;
         public static int TotalQuestions;
+        
+        public static float MaxHealth = 1000;
+        public static float CurrentHealth;
+        public static float EnemyDamage = 5;
     }
 }
--- Assets/Scripts/FightScene/DateSiteBehavior.cs
+++ Assets/Scripts/FightScene/DateSiteBehavior.cs
@@ -1,19 +1,40 @@
 ﻿using System;
+using DefaultNamespace;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class DateSiteBehavior : MonoBehaviour
 {
+    private bool gameLost;
+    
     private void Awake()
     {
         GetComponent<Collider2D>().isTrigger = true;
+        Data.CurrentHealth = Data.MaxHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Debug.Log("Date Site Attacked!");
+            Debug.Log($"Date Site Attacked! {Data.EnemyDamage} hp hit!");
         }
     }
+
+    void AcceptDamage(float damageAmount)
+    {
+        if (gameLost) return;
+
+        Data.CurrentHealth -= damageAmount;
+
+        if (Data.CurrentHealth <= 0)
+        {
+            // death event
+            gameLost = true;
+            return;
+        }
+        
+        // Hit event
+    }
+
 }
